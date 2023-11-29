1124d34855e36b10d0bba5e50993fcb389fff4f0
Yury V. Zaytsev <yury.zaytsev@moneymeets.com>
2023-11-29 15:47:03 +0100

    refactor(views): make a package out of a single module

--- sepacetamol/urls.py
+++ sepacetamol/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from . import views
+from .views import sepa
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("generate/", views.generate, name="generate"),
+    path("", sepa.index, name="index"),
+    path("generate/", sepa.generate, name="generate"),
 ]
